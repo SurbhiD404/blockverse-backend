a5b73c213790073a9672e8c196fca6f826419c28
unknown <dharvansurbhi@gmail.com>
2026-02-06 19:04:37 +0530

    Auto create admin

--- build.sh
+++ build.sh
@@ -8,3 +8,14 @@ python manage.py migrate
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+
+
+
+
+
+
+echo "from django.contrib.auth import get_user_model;
+User = get_user_model();
+User.objects.filter(username='admin').exists() or
+User.objects.create_superuser('admin', 'surbhidharvan@gmail.com', 'admin123')" | python manage.py shell
+
