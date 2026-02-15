cd4a4bb84e7f21d04761db76d9ae373e6ffac88e
unknown <dharvansurbhi@gmail.com>
2026-02-15 14:41:42 +0530

    Add email timeout

--- blockverse/settings.py
+++ blockverse/settings.py
@@ -175,6 +175,7 @@ CSRF_COOKIE_SECURE = not DEBUG
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_TIMEOUT = 5 
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 
 EMAIL_HOST_USER = os.getenv("EMAIL_USER")
